d5a80fb06b7afec91d47e4fbf00a19e88a9422ee
kinguu <bonakinga4@gmail.com>
2017-10-24 21:14:21 +0300

    uj2

--- app/src/main/java/com/example/dell/learnaboutromania/SecondActivity.java
+++ app/src/main/java/com/example/dell/learnaboutromania/SecondActivity.java
@@ -19,7 +19,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonRegister;
     private EditText editTextEmail;
@@ -34,46 +34,49 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        firebaseAuth= FirebaseAuth.getInstance();
-        buttonRegister=(Button)findViewById(R.id.buttonRegister);
-        editTextEmail=(EditText) findViewById(R.id.editTextEmail);
-        editTextPassword=(EditText) findViewById(R.id.editTextPassword);
-        TextViewSignin=(TextView) findViewById(R.id.textViewSignin);
-        progressDialog= new ProgressDialog(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        TextViewSignin = (TextView) findViewById(R.id.textViewSignin);
+        progressDialog = new ProgressDialog(this);
 
         buttonRegister.setOnClickListener(this);
         TextViewSignin.setOnClickListener(this);
 
     }
 
-    private void registerUser(){
-        String email=editTextEmail.getText().toString().trim();
-        String password=editTextPassword.getText().toString().trim();
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
 
-        if(TextUtils.isEmpty(email)){
-         //nu au scris e-mail
-            Toast.makeText(this, "Enter you e-mail",Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(email)) {
+            //nu au scris e-mail
+            Toast.makeText(this, "Enter you e-mail", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        if(TextUtils.isEmpty(password)){
+        if (TextUtils.isEmpty(password)) {
             // nu au scris parola
-            Toast.makeText(this, "Enter you password",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Enter you password", Toast.LENGTH_SHORT).show();
             return;
 
         }
 
         progressDialog.setMessage("Registering..");
         progressDialog.show();
-        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful())
-                {
-                    Toast.makeText(SecondActivity.this, "Registered successfully",Toast.LENGTH_SHORT).show();
-            }
-            else
-                { Toast.makeText(SecondActivity.this, "Error",Toast.LENGTH_SHORT).show();
+                if (task.isSuccessful()) {
+                    Toast.makeText(SecondActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                } else {
+                    Toast.makeText(
+                            SecondActivity.this,
+                            "Error: " + task.getException().getMessage(),
+                            Toast.LENGTH_SHORT
+                    ).show();
                 }
             }
         });
@@ -81,19 +84,15 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
     }
 
 
+    @Override
+    public void onClick(View v) {
 
-
-
-
-        @Override
-        public void onClick(View v) {
-
-            if(v==buttonRegister){
-                  registerUser();
+        if (v == buttonRegister) {
+            registerUser();
         }
 
-        if(v==TextViewSignin)
-        {  // THE LOGIN ACTIVITY
+        if (v == TextViewSignin) {  // THE LOGIN ACTIVITY
 
-}}
+        }
+    }
 }
