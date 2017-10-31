9dc198fb5949e6f3a8f6a11ef255991bdac312c9
kinguu <bonakinga4@gmail.com>
2017-10-31 20:40:30 +0200

    history

--- app/src/main/java/com/example/dell/learnaboutromania/History.java
+++ app/src/main/java/com/example/dell/learnaboutromania/History.java
@@ -6,27 +6,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
 
 public class History extends AppCompatActivity {
-    private CheckBox checkbox;
+    private RadioButton radiobutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-        checkbox = (CheckBox) findViewById(R.id.checkBox);
-        checkbox.setOnClickListener(new View.OnClickListener() {
+        radiobutton = (RadioButton) findViewById(R.id.InEnglishBtn);
+        radiobutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkbox.isChecked()) {
+                if (radiobutton.isChecked()) {
+
                     String history = "https://en.wikipedia.org/wiki/History_of_Romania";
                     Uri webaddress = Uri.parse(history);
                     Intent gotoWikipedia = new Intent(Intent.ACTION_VIEW, webaddress);
+                    startActivity(gotoWikipedia);
                 } else {
                     String history = "https://ro.wikipedia.org/wiki/Istoria_Rom%C3%A2niei";
                     Uri webaddress = Uri.parse(history);
 
                     Intent gotoWikipedia = new Intent(Intent.ACTION_VIEW, webaddress);
+                    startActivity(gotoWikipedia);
                 }
             }
         });
